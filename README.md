94e732085a389eb1b02f5dd5044097e198f87064
unix121 <grigorio@ceid.upatras.gr>
2017-04-24 19:06:35 +0300

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# sysinfo-minimal
+<h1>sysinfo-minimal</h1>
 Simple bash script to display system information in a minimalistic way.
+
+<ul>
+<h2> Screenshots </h2>
+<img src="https://github.com/unix121/sysinfo-minimal/blob/master/screenshots/sysinfo-minimal-1.png?raw=true>
+
+<img src="https://github.com/unix121/sysinfo-minimal/blob/master/screenshots/sysinfo-minimal-2.png?raw=true>
+</ul>
+
+<ul>
+<h2>How to use</h2>
+<li> git clone https://github.com/unix121/sysinfo-minimal</li>
+<li> cd sysinfo-minimal/source/</li>
+<li> ./sysinfo-minimal.sh</li>
+</ul>
---
+++ source/sysinfo-minimal.sh
@@ -0,0 +1,37 @@
+#/usr/bin/env bash
+
+# Author : Unix121
+# GitHub : github.com/unix121
+# E-mail : unix121@protonmail.com
+# Description : Simple bash script that displays some 
+#               system information using the terminal
+#               colors. Designed to be minimalistic and
+#               easy to configure/change.
+
+printf -v color0 %b "\e[30m"
+printf -v color1 %b "\e[31m"
+printf -v color2 %b "\e[32m"
+printf -v color3 %b "\e[33m"
+printf -v color4 %b "\e[34m"
+printf -v color5 %b "\e[35m"
+printf -v color6 %b "\e[36m"
+
+bold=$(tput bold)
+normal=$(tput sgr0)
+
+user=$(whoami)
+linux=$(lsb_release -is)
+kernel=$(uname -r)
+bash=$BASH_VERSION
+wm=$GDMSESSION
+term=$TERM
+
+echo
+echo $color1 ████████████████ ${bold}User:${normal}  $user 
+echo $color2 ████████████████ ${bold}Distro:${normal} $linux
+echo $color3 ████████████████ ${bold}Kernel:${normal} $kernel
+echo $color4 ████████████████ ${bold}Shell:${normal} $BASH_VERSION
+echo $color5 ████████████████ ${bold}WM:${normal} $wm
+echo $color6 ████████████████ ${bold}Terminal:${normal} $term
+echo
+
